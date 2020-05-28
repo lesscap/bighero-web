6f2f43861e3ffb92b9eee522cd1c37eb2ef7d627
bencode <bencode@163.com>
2020-05-28 21:25:48 +0800

    add style

--- lib/bighero_web/controllers/page_controller.ex
+++ lib/bighero_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule BigheroWeb.PageController do
   use BigheroWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", assets: "index")
   end
 end
