0d93d2d40fb6f85ab28e6a36eb75b5e3c50148b4
bencode <bencode@163.com>
2020-05-28 21:20:58 +0800

    page sketch

--- assets/src/pages/index/index.js
+++ assets/src/pages/index/index.js
@@ -1,1 +1,10 @@
-console.log('hello world!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello</div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
