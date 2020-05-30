7577d8eb55ad1f6b834e33af9f7c226028df7ce3
bencode <bencode@163.com>
2020-05-30 23:24:57 +0800

    修改看部署

--- assets/src/pages/index/index.js
+++ assets/src/pages/index/index.js
@@ -4,7 +4,9 @@ import style from './style.less';
 
 const App = () => {
   return (
-    <div className={style.app}>Hello</div>
+    <div className={style.app}>
+      <h1>鱼米OKR</h1>
+    </div>
   );
 };
 
